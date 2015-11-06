a2f853f3665d140b167c84af36a4a23dda5a23c5
David Villegas Aguilar <david.villegas.aguilar@gmail.com>
2015-11-06 19:19:27 -0300

    Aplicacion de Suma y Resta

---
+++ SantoTomasFundamentos/src/com/santotomas/App01/Resta.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.santotomas.App01;
+
+/**
+ * @author David
+ *
+ */
+public final class Resta extends Operacion{
+
+	public void operar(){
+        resultado=valor1-valor2;
+    }
+}
---
+++ SantoTomasFundamentos/src/com/santotomas/App01/Suma.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.santotomas.App01;
+
+/**
+ * @author David
+ *
+ */
+public final class Suma extends Operacion {
+
+	void operar() {
+        resultado=valor1+valor2;
+    }
+}
