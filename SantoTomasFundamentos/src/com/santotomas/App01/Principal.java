da4159cc08ed58168bf5fb26a943377a2b560be1
David Villegas Aguilar <david.villegas.aguilar@gmail.com>
2015-11-06 21:39:20 -0300

    Actualizacion

--- SantoTomasFundamentos/src/com/santotomas/App01/Principal.java
+++ SantoTomasFundamentos/src/com/santotomas/App01/Principal.java
@@ -13,12 +13,7 @@ public class Principal {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	        Suma suma1=new Suma();
-	        suma1.cargar1();
-	        suma1.cargar2();
-	        suma1.operar();
-	        System.out.print("El resultado de la suma es:");
-	        suma1.mostrarResultado();
+	    
 	        Resta resta1=new Resta();
 	        resta1.cargar1();
 	        resta1.cargar2();
